3cf787e02242c385514dd3286969758809a910e3
Pablo Baltodano Madrigal <Pablo Baltodano Madrigal>
2016-10-12 19:38:49 -0600

    Tab Cotizaciones con correcciones

--- cotizaciones.php
+++ cotizaciones.php
@@ -3,6 +3,7 @@
 <head>
   <title>M&M Soluciones</title>
   <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="estilos/estilos.css">
   <link rel="stylesheet" type="text/css" href="estilos/styles_cotizaciones.css">
   <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
   <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
@@ -15,11 +16,11 @@
       </div>
       <div id="menu">
           <ul>
-            <li><a href="#">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="menu.php">Menu</a></li>
             <li><a href="#">Servicios</a></li>
             <li class="current_page_item"><a href="#">Cotizaciones</a></li>
-            <li><a href="#">Contacto</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
             <li><a href="#">Nosotros</a></li>
         </ul>
       </div>
